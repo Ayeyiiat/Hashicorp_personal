c31ad90099e1e03dbba8aff50af874cd5a819ea8
Ayeyi_iat <asaturks24@gmail.com>
2022-05-18 17:38:43 -0400

    Started looking into hcdiag

--- GO/hello.go
+++ GO/hello.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().Unix()) //Chnaging the seed value every run so as to get a different favorite number
 	fmt.Println("Hello, Ayeyi.")
 	fmt.Println("Welcome to Go.")
 	fmt.Println("The time is now", time.Now())
+	fmt.Println("My favorite random number is", rand.Intn(10))
 }
